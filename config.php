d69cf64a055151e1da4881107e568694453f3192
Radon <46275386+TornadoRadon@users.noreply.github.com>
2019-12-14 12:26:00 +0500

    min config

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by Radon
+ * Date: 12/14/2019
+ * Time: 12:18 PM
+ */
+
+const DB_ = [
+    "host" => 'localhost',
+    "db"   => 'openuz',
+    "user" => 'root',
+    "pass" => ''
+];
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by Radon
+ * Date: 12/14/2019
+ * Time: 12:15 PM
+ */
+
+include_once("config.php");
+
+$db = new PDO("mysql:host=".DB_['host'].";dbname=".DB_['db'], DB_['user'], DB_['pass']);
+
+?>
+<!doctype html>
+<html lang="uz">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Open uzbekistan</title>
+</head>
+<body>
+    
+</body>
+</html>
