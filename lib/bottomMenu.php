95d72acf7c1273a8c1a6f941b7ea7dbf08148c8f
Oxunjon <36882108+okhun9494@users.noreply.github.com>
2019-12-15 13:43:51 +0500

    Update bottomMenu.php

--- lib/bottomMenu.php
+++ lib/bottomMenu.php
@@ -15,50 +15,50 @@
                 <div class="indicImg transition">
                     <img src="img/indicator/unemployment.png">
                 </div>
-                <div class="indicName"><span>Unemployment</span></div>
+                <div class="indicName"><span>Ishsizlik</span></div>
             </a>
         </li>
         <li id="inflation" class="indicBtn" data-link="inflation">
             <div class="indicImg transition">
                 <img src="img/indicator/inflation.png">
             </div>
-            <div class="indicName"><span>Inflation</span></div>
+            <div class="indicName"><span>Inflatsiya</span></div>
         </li>
         <li id="production" class="indicBtn" data-link="production">
             <div class="indicImg transition">
                 <img src="img/indicator/production.png">
             </div>
-            <div class="indicName"><span>Industrial production</span></div>
+            <div class="indicName"><span>Import</span></div>
         </li>
         <li id="deficit" class="indicBtn" data-link="deficit">
             <div class="indicImg transition">
                 <img src="img/indicator/deficit.png">
             </div>
-            <div class="indicName"><span>Government deficit/surplus</span></div>
+            <div class="indicName"><span>Eksport</span></div>
         </li>
         <li id="debt" class="indicBtn" data-link="debt">
             <div class="indicImg transition">
                 <img src="img/indicator/debt.png">
             </div>
-            <div class="indicName"><span>Government debt</span></div>
+            <div class="indicName"><span>Davlat qarzi</span></div>
         </li>
         <li id="labour" class="indicBtn" data-link="labour">
             <div class="indicImg transition">
                 <img src="img/indicator/labour.png">
             </div>
-            <div class="indicName"><span>Labour cost</span></div>
+            <div class="indicName"><span>Investitsiya</span></div>
         </li>
         <li id="gdp" class="indicBtn" data-link="gdp">
             <div class="indicImg transition">
                 <img src="img/indicator/gdp.png">
             </div>
-            <div class="indicName"><span>GDP</span></div>
+            <div class="indicName"><span>YAIM</span></div>
         </li>
         <li id="gdphab" class="indicBtn" data-link="gdphab">
             <div class="indicImg transition">
                 <img src="img/indicator/gdphab.png">
             </div>
-            <div class="indicName"><span>GDP per inhabitant</span></div>
+            <div class="indicName"><span>Boshqa iqtisodiy ko'rsatkichlar</span></div>
         </li>
     </ul>
 </div>
