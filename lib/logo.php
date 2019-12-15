7fefb9b5bbff113435afefe49ebdcf16902b06aa
Oxunjon <36882108+okhun9494@users.noreply.github.com>
2019-12-15 11:45:23 +0500

    Update logo.php

--- lib/logo.php
+++ lib/logo.php
@@ -1,1 +1,3 @@
-
+  <div id="logo" class="transition">
+            <a href="index.php" title="" style="background: url(&quot;img/logoDesktop.png&quot;) left top / 190px 55px no-repeat;"></a>
+        </div>
