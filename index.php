1055cc16da8ce279342d06893c6dff3493f34fb9
Radon <46275386+TornadoRadon@users.noreply.github.com>
2019-12-15 11:09:30 +0500

    some changes

--- index.php
+++ index.php
@@ -94,59 +94,8 @@
 
             <div id="countryClose"></div>
         </div>
-        <div id="introChoice">Statistik ma'lumot 2019</div>
-        <div id="indicWrap">
-            <ul>
-                <li id="unemployment" class="indicBtn" data-link="unemployment" onclick="alert('Клик!')">
-                    <div class="indicImg transition">
-                        <img src="img/indicator/unemployment.png">
-                    </div>
-                    <div class="indicName"><span>Ishsizlik</span></div>
-                </li>
-                <li id="inflation" class="indicBtn" data-link="inflation">
-                    <div class="indicImg transition">
-                        <img src="img/indicator/inflation.png">
-                    </div>
-                    <div class="indicName"><span>Inflation</span></div>
-                </li>
-                <li id="production" class="indicBtn" data-link="production">
-                    <div class="indicImg transition">
-                        <img src="img/indicator/production.png">
-                    </div>
-                    <div class="indicName"><span>Industrial production</span></div>
-                </li>
-                <li id="deficit" class="indicBtn" data-link="deficit">
-                    <div class="indicImg transition">
-                        <img src="img/indicator/deficit.png">
-                    </div>
-                    <div class="indicName"><span>Government deficit/surplus</span></div>
-                </li>
-                <li id="debt" class="indicBtn" data-link="debt">
-                    <div class="indicImg transition">
-                        <img src="img/indicator/debt.png">
-                    </div>
-                    <div class="indicName"><span>Government debt</span></div>
-                </li>
-                <li id="labour" class="indicBtn" data-link="labour">
-                    <div class="indicImg transition">
-                        <img src="img/indicator/labour.png">
-                    </div>
-                    <div class="indicName"><span>Labour cost</span></div>
-                </li>
-                <li id="gdp" class="indicBtn" data-link="gdp">
-                    <div class="indicImg transition">
-                        <img src="img/indicator/gdp.png">
-                    </div>
-                    <div class="indicName"><span>GDP</span></div>
-                </li>
-                <li id="gdphab" class="indicBtn" data-link="gdphab">
-                    <div class="indicImg transition">
-                        <img src="img/indicator/gdphab.png">
-                    </div>
-                    <div class="indicName"><span>GDP per inhabitant</span></div>
-                </li>
-            </ul>
-        </div>
+        <!--        Bottom menu-->
+        <?php include_once ("lib/bottomMenu.php");?>
         <div id="logo" class="transition">
             <a href="index.html" title="" style="background: url(&quot;img/logoDesktop.png&quot;) left top / 190px 55px no-repeat;"></a>
         </div>
